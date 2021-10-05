55bea3ea484cfe9421337660064a5ae020881b05
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-10-05 11:08:16 +0200

    fighting

---
+++ Bossfight/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+string[] names = {"Joachim", "Mermelaine"};
+
+int[] arrayHp = new int[2];
+
+
+
+Random generator = new Random();
+
+int damage = generator.Next(10, 30);
+
+System.Console.WriteLine(damage);
+
+
+
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System.IO;
+
+//string art = File.ReadAllText("artwork.txt");
+
+//System.Console.WriteLine(art);
+
+//Console.ReadLine();
+
