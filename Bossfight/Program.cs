94381094fc5e3bf35d97b7aa4ba0d28c0640dc7c
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-10-06 11:05:30 +0200

    fithing

--- Bossfight/Program.cs
+++ Bossfight/Program.cs
@@ -1,18 +1,34 @@
 ﻿using System;
 
 
-string[] names = {"Joachim", "Mermelaine"};
+//string[] names = {"Joachim", "Mermelaine"};
 
-int[] arrayHp = new int[2];
+//int[] arrayHp = new int[2];
 
 
 
 Random generator = new Random();
 
 int damage = generator.Next(10, 30);
+int health = 100;
+int fighter1 = health;
+int fighter2 = health;
 
-System.Console.WriteLine(damage);
+while (health != 0)
+{
+System.Console.WriteLine(health);
+health -= damage;
+Console.ReadLine();
 
+if (health < 1)
+{
+System.Console.WriteLine("fight one wins");
+Console.ReadLine();
+
+
+}
+
+}
 
 
 
@@ -54,6 +70,12 @@ Console.ReadLine();
 
 
 
+
+//for (int health = 100; health > 0; health -= damage)
+//{
+//    damage = generator.Next(10, 30);
+//   System.Console.WriteLine(health);
+//}
 
 
 //using System.IO;
