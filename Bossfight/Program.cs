0c82f9dd8ae686b049174cdfd0532ed71364c27e
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2021-10-12 11:03:45 +0200

    verypog

--- Bossfight/Program.cs
+++ Bossfight/Program.cs
@@ -1,30 +1,56 @@
 ﻿using System;
+using System.IO;
 
-
-//string[] names = {"Joachim", "Mermelaine"};
-
-//int[] arrayHp = new int[2];
 
 
 
 Random generator = new Random();
 
-int damage = generator.Next(10, 30);
 int health = 100;
-int fighter1 = health;
-int fighter2 = health;
+int fighter1Health = health;
+int fighter2Health = health;
+string art = File.ReadAllText("artwork.txt");
+string art1 = File.ReadAllText("artworklose.txt");
+string art2 = File.ReadAllText("artworkwin.txt");
 
-while (health != 0)
+while (fighter1Health > 0 && fighter2Health > 0)
 {
-System.Console.WriteLine(health);
-health -= damage;
+    
+
+
+System.Console.WriteLine(art);
+
 Console.ReadLine();
 
-if (health < 1)
-{
-System.Console.WriteLine("fight one wins");
+int damage = generator.Next(1, 20);
+System.Console.WriteLine($"Fighter 1 health {fighter1Health}");
+fighter1Health -= damage;
+
+damage = generator.Next(1, 20);
+fighter2Health -= damage;
+System.Console.WriteLine($"Fighter 2 health {fighter2Health}");
 Console.ReadLine();
 
+if (fighter1Health < 0)
+{
+System.Console.WriteLine("Fighter 2 wins");
+Console.ReadLine();
+System.Console.WriteLine(art1);
+
+Console.ReadLine();
+
+
+
+}
+
+
+else if (fighter2Health < 0)
+{
+System.Console.WriteLine("fighter 1 wins");
+Console.ReadLine();
+System.Console.WriteLine(art2);
+
+Console.ReadLine();
 
 }
 
@@ -32,7 +58,6 @@ Console.ReadLine();
 
 
 
-Console.ReadLine();
 
 
 
@@ -64,10 +89,9 @@ Console.ReadLine();
 
 
 
+//string[] names = {"Joachim", "Mermelaine"};
 
-
-
-
+//int[] arrayHp = new int[2];
 
 
 
